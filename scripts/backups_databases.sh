65b5b16522a03e78e0f365a6f3de4da0f33770a2
Juan Bermúdez <juangbermudezo@gmail.com>
2024-11-10 23:03:41 -0500

    Script para backup de las bases de datos MySQL

--- scripts/backups_databases.sh
+++ scripts/backups_databases.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+#Repite este proceso dentro de este script si tienes diferentes bases de datos.
 
 USER="usuario"
 PASSWORD="contraseña"
 DATABASE="base-de-datos"
-
 FINAL_OUTPUT=/ruta-destino/`date +%Y%m%d`_base-de-datos.sql
 mysqldump --user=$USER --password=$PASSWORD $DATABASE > $FINAL_OUTPUT
