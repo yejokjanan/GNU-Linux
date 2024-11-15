11aba3268bff09a605fbc7b44f62fa58d38059e5
Juan Bermúdez <juangbermudezo@gmail.com>
2024-11-14 19:14:26 -0500

    backups_server.sh

--- scripts/backups_server.sh
+++ scripts/backups_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Para hacer más fácil la copia automática ejecute:
+# ssh-keygen
+# ssh-copy-id usuario@host
+# Agregue este script a crontab -e
 
 rsync --delete -avhzrP -e "ssh -p #puerto" usuario@host:/ruta-origen/ /ruta-destino/
